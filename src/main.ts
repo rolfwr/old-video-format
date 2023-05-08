38b3259c55aabe80e61e696a468a7adbfc5d56a6
Rolf W. Rasmussen <rolfwr@gmail.com>
2023-05-09 00:20:36 +0200

    Reset

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,10 @@
 import { slide00 } from "./slide00";
-import { slide01 } from "./slide01";
-import { slide02 } from "./slide02";
-import { slide03 } from "./slide03";
-import { slide04 } from "./slide04";
-import { slide05 } from "./slide05";
-import { slide06 } from "./slide06";
-import { slide07 } from "./slide07";
-import { slide08 } from "./slide08";
-import { slide09 } from "./slide09";
-import { slide10 } from "./slide10";
-import { slide11 } from "./slide11";
 
 async function main() {
   const doc = window.document;
   const parent = doc.getElementById('scratch') as HTMLElement;
 
-  await slide11(parent);
+  await slide00(parent);
 }
 
 main();
